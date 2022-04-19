d3eb9bf6a57eba34bb2a062d023819e08dfbfb5f
이승헌 <rltmsla5@naver.com>
2022-04-19 16:32:54 +0900

    041911
    041911

--- 3TeamProject/Assets/04.Script/ChangScenes.cs
+++ 3TeamProject/Assets/04.Script/ChangScenes.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangScenes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChangSceneBtn() 
     {
-        
-    }
+        switch (this.gameObject.name)
+        {
+            case "Bt0":
+                SceneManager.LoadScene("Scin0");
+                Debug.Log ("Click0");
+                break;
+            case "Bt1":
+                SceneManager.LoadScene("Scin1");
+                Debug.Log("Click1");
+                break;
+            case "Bt2":
+                SceneManager.LoadScene("Scin2");
+                Debug.Log("Click2");
+                break;
+            case "Bt3":
+                SceneManager.LoadScene("Scin3");
+                Debug.Log("Click3");
+                break;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        }
     }
 }
